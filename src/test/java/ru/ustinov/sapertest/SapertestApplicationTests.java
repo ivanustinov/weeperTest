3678b60abc084c7bbd64214d755725eda8a10175
Ivan <ivanustinov1985@gmail.com>
2024-02-09 10:14:19 +0500

    init commit

---
+++ src/test/java/ru/ustinov/sapertest/SapertestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ustinov.sapertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapertestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
